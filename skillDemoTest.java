9dc92bf0940422f50bb4c629d38c0cfb7f42e26e
reisandylamdjani <lamdjani@gmail.com>
2022-05-10 23:01:28 -0700

    pass test

--- skillDemoTest.java
+++ skillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class skillDemoTest {
     @Test
     public void addition() {
-        assertEquals(2, skillDemo.add(2, 1)); //fail initially
+        assertEquals(2, skillDemo.add(1, 1)); //pass test now
     }
 }
