33dede5c7abbb5265cb9896dea1b238c45db3664
reisandylamdjani <lamdjani@gmail.com>
2022-05-10 22:59:49 -0700

    new test file

--- skillDemo.java
+++ skillDemo.java
@@ -1,4 +1,7 @@
 public class skillDemo {
-
+    public static int add(int m, int n)
+    {
+        return m + n;
+    }
 
 }
---
+++ skillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemoTest {
+    @Test
+    public void addition() {
+        assertEquals(2, skillDemo.add(2, 1)); //fail initially
+    }
+}
